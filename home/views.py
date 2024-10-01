ac0e575e08e0b7c7ab626a6f6d6451b8b2904a52
Anuj rathore <yugratore457@gmail.com>
2024-10-01 12:46:05 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate ,logout
+from django.contrib.auth import login as dj_login
+from django.contrib.auth.models import User
+from .models import Addmoney_info,UserProfile
+from django.contrib.sessions.models import Session
+from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
+from django.db.models import Sum
+from django.http import JsonResponse
+import datetime
+from django.utils import timezone
